6bcb81c174d71a3cd7def9c90b7d928978b1686a
Spencer Rosio <spencerrosio3d@gmail.com>
2021-08-16 21:33:35 -0500

    okta oauth

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,55 @@ import ProjectionPage from "./components/ProjectionPage";
 import TransactionsPage from "./components/TransactionsPage";
 import TransfersPage from "./components/TransfersPage";
 import AccountsPage from "./components/AccountsPage";
+import { OktaAuth } from "@okta/okta-auth-js";
 import './App.css';
+const BASE_PATH =  process.env.BASE_PATH || "http://localhost:3001/";
+
+const OktaAuthClient = new OktaAuth({
+  scopes: [
+    "openid",
+    "profile",
+    "email"
+  ],
+  issuer: "https://dev-99014916.okta.com/oauth2/default",
+  clientId: "0oa1gzxlndwFvyyCa5d7",
+  redirectUri: BASE_PATH,
+  tokenManager: {
+    storage: "localStorage",
+    expireEarlySeconds: 600,
+    autoRenew: true
+  },
+  responseType: [
+    'id_token',
+    'token'
+  ],
+  pkce: false
+});
 
 function App() {
+
+  const [authorized, setAuthorised] = useState(false);
+
+  async function checkAuthorization(){
+    let oToken = await OktaAuthClient.tokenManager.getTokens();
+    // Add token to context and share with API calls
+    console.log("Token", oToken);
+    if(Object.keys(oToken).length  === 0){
+      console.log("No token found");
+      if(window.location.href.indexOf("access_token")>-1){
+        let { tokens } = await OktaAuthClient.token.parseFromUrl()
+        console.log("tokens",tokens);
+        OktaAuthClient.tokenManager.setTokens(tokens);
+      } else {
+        OktaAuthClient.token.getWithRedirect({});
+      }
+    }
+    setAuthorised(true);
+
+  }
+
+  checkAuthorization();
+
   const [activePage, setActivePage] = useState("projection");
 
   // -------------------------
@@ -34,6 +80,14 @@ function App() {
     if(activePage === "accounts"){
       return <AccountsPage />
     }
+  }
+
+  if(!authorized){
+    return (
+      <div className="App">
+        ...redirecting
+      </div>
+    )
   }
 
   return (
